0402ac164e1fa5fd61e2814c156c5a47500c58b0
Zhongyu Wang <zhongyuzone@gmail.com>
2018-03-20 15:52:47 +0100

    Add gps test program

---
+++ waypoint_nav/src/test.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python 
+ 
+from pyproj import Proj 
+ 
+projection = Proj(proj="utm", zone="34", ellps='WGS84') 
+ 
+x = 1 
+y = 0 
+ 
+lon,lat = projection(x, y, inverse=True) 
+ 
+print lon 
+print lat 
